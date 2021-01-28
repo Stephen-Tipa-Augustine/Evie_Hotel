ea2a4361413e021802fecd330b71f2f81c1413d8
Stephen Tipa Augustine <tipastep5@gmail.com>
2021-01-28 19:30:54 +0300

    first commit

---
+++ Evie_Hotel/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Evie_Hotel project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Evie_Hotel.settings')
+
+application = get_asgi_application()
